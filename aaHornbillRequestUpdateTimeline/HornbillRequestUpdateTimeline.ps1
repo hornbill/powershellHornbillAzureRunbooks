7c835ef2793c9921e38ac462b2bff7bb450334cd
Steve Goldthorpe <steve.goldthorpe@hornbill.com>
2020-06-12 11:54:32 +0100

    v1.1.1

--- aaHornbillRequestUpdateTimeline/HornbillRequestUpdateTimeline.ps1
+++ aaHornbillRequestUpdateTimeline/HornbillRequestUpdateTimeline.ps1
@@ -1,7 +1,7 @@
 
 <#PSScriptInfo
 
-.VERSION 1.1.0
+.VERSION 1.1.1
 
 .GUID 2b3d9b2a-2557-48a5-a3b0-68fb16c190d7
 
@@ -18,12 +18,43 @@
 .ICONURI https://wiki.hornbill.com/skins/common/images/HBLOGO.png
 
 .RELEASENOTES
-Removed requirement to provide instanceZone param
+Corrected metadata
+Included parameter descriptions
 
 .DESCRIPTION
  Azure Automation Runbook to post an update to the timeline of a Request within Service Manager on a Hornbill instance.
 
 #>
+
+#.PARAMETER instanceName
+#MANDATORY: The name of the Instance to connect to.
+
+#.PARAMETER instanceKey
+#MANDATORY: An API key with permission on the Instance to carry out the required API calls.
+
+#.PARAMETER requestReference
+#MANDATORY: The request reference ID
+
+#.PARAMETER action
+#An action name for the operation.
+
+#.PARAMETER source
+#Source (device type or logical) where the update was performed Example: webclient, email, mobile...
+
+#.PARAMETER content
+#MANDATORY: The update text itself. This is the main body of the post to the request timeline.
+
+#.PARAMETER extra
+#Extra details for the update. Is a well formatted JSON String.
+
+#.PARAMETER visibility
+#Visibility for the post - the default is trustedGuest.
+
+#.PARAMETER imageUrl
+#The url of an image to be posted as part of the update.
+
+#.PARAMETER activityType
+#The type of activity. When querying activities this can be used to filter activities.
 
 #Requires -Module @{ModuleVersion = '1.1.0'; ModuleName = 'HornbillAPI'}
 #Requires -Module @{ModuleVersion = '1.1.1'; ModuleName = 'HornbillHelpers'}
